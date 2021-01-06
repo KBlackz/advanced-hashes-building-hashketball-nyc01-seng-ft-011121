18ec7afd6a2789e98f6f36cddd4d91aa3e98b3ac
Karris  Black <karris.b@gmail.com>
2021-01-06 23:40:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -72,7 +72,7 @@ def game_hash
            :points => 10,
            :rebounds => 1,
            :assists => 1,
-           :steals => 1,
+           :steals => 2,
            :blocks => 7,
            :slam_dunks => 2
           },
